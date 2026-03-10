bcecd37b76588a498c69ab4cd0b029615ddc868c
lounisyounes <youneslounis887@gmail.com>
2026-03-10 11:19:59 -0400

    Creation de la classe Voiture

---
+++ Atelier_3.py
@@ -0,0 +1,10 @@
+class Voiture:
+    def __init__(self, matricule, marque, couleur):
+        self.matricule = matricule
+        self.marque = marque
+        self.couleur = couleur
+
+    def afficher_infos(self):
+        print("Matricule :", self.matricule)
+        print("Marque :", self.marque)
+        print("Couleur :", self.couleur)
